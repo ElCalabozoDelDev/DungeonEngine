9437c00bb3a69cea51b5be8d089eec6f440f1ed2
Francisco Javier Bahamondes Castro <francisco.bahamondes90@gmail.com>
2024-08-15 19:42:30 -0400

    cmake and main work

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -9,7 +9,7 @@ and may not be redistributed without written permission.*/
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
-int main( int argc, char* args[] )
+int WinMain( int argc, char* args[] )
 {
 	//The window we'll be rendering to
 	SDL_Window* window = NULL;
