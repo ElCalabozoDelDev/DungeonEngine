96f7bea0bbd173aac3e9959e8453907940a932f3
Francisco Bahamondes <francisco.bahamondes90@gmail.com>
2024-09-05 17:58:39 -0400

    clean widget system

--- game/src/systems/widget_system.cpp
+++ game/src/systems/widget_system.cpp
@@ -6,26 +6,6 @@
 void WidgetSystem::run(entt::registry &registry) {
     // Obtener el renderer de SDL
     SDL_Renderer *renderer = registry.ctx().get<SDL_Renderer *>();
-    // Crear una ventana de ImGui para mostrar la información del sistema
-    // auto view = registry.view<AnimationComponent, TextureComponent>();
-    // for (auto entity : view) {
-    //     auto& animation = view.get<AnimationComponent>(entity);
-    //     auto& texture = registry.get<TextureComponent>(entity);
-
-    //     ImGui::Begin("Estado de la textura");
-    //     ImGui::Text("spriteRow: %d", texture.spriteRow);
-    //     ImGui::Text("spriteCol: %d", texture.spriteCol);
-    //     ImGui::Text("currentSprite: %d", animation.currentSprite);
-    //     ImGui::Text("currentFrame: %d", animation.currentFrame);
-    //     ImGui::Text("totalFrames: %d", animation.totalFrames);
-    //     ImGui::Text("animationTime: %.2f", animation.animationTime);
-    //     ImGui::Text("timeSinceLastFrame: %.2f", animation.timeSinceLastFrame);
-    //     ImGui::End();
-    // }
-    // Actualiza el frame del componente CounterGui
-    // counter_gui.frame_begin();          // Resetea hooks
-    // counter_gui.frame_update(registry); // Actualiza el renderizado
-    // counter_gui.frame_end();            // Aplica las mutaciones (si las hay)
     registry.view<std::unique_ptr<gui::WidgetComponent>>().each([&registry](auto entity, auto& widget_component) {
         widget_component->frame_begin();
         widget_component->frame_update(registry);
