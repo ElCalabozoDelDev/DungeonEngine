2514a03e1c50de218e80efc3b278d208e0fa1dbd
Francisco Bahamondes <francisco.bahamondes90@gmail.com>
2024-10-16 09:33:18 -0300

    si

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -1,12 +1,4 @@
 #define SDL_MAIN_HANDLED
-// #include "core/game.hpp"
-
-// int main( int argc, char* args[] )
-// {
-//     Game::Instance()->init("../assets/game.xml");
-//     Game::Instance()->run();
-//     return 0;
-// }
 
 #include "core/game_loop.hpp"
 #include "loaders/config.hpp"
